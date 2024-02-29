0682446c52305f109b30d95721ad0bdac03c738f
Adlin Mohamad Hasri <adlin@rushowl.sg>
2024-02-29 23:55:40 +0700

    style: update font - use font-gilroy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,46 @@
-import "./globals.css";
+import "../styles/globals.css";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+// import { Roboto } from "next/font/google";
 import { Layout } from "@/components";
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700", "900"],
-  display: "swap",
-});
+// const roboto = Roboto({
+//   subsets: ["latin"],
+//   weight: ["300", "400", "500", "700", "900"],
+//   display: "swap",
+// });
+
+import localFont from 'next/font/local'
+
+// Font files can be colocated inside of `app`
+const myFont = localFont({
+  src: [
+    {
+      path: '../styles/fonts/Gilroy-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/Gilroy-RegularItalic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../styles/fonts/Gilroy-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/Gilroy-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-gilroy',
+})
 
 export const metadata: Metadata = {
-  title: "NextJS Tailwind Course Landing Page",
+  title: "NextJS Tailwi nd Course Landing Page",
   description:
     "Introducing Tailwind Course Landing Page, a versatile and engaging landing page template designed using Tailwind CSS and Material Tailwind.",
 };
@@ -27,7 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
 
-      <body className={roboto.className}>
+      <body className={myFont.className}>
         <Layout>
           {children}
           {/* <FixedPlugin /> */}
