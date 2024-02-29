7f62466e692b788777dcf1ff3c366ef80be98666
Adlin Mohamad Hasri <adlin@rushowl.sg>
2024-02-29 22:59:58 +0700

    style: setup button theme - primary

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,14 @@
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
+import buttonTheme from '@/themes/button'
+
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  const theme = {
+    button: { ...buttonTheme },
+  };
+
+  return <ThemeProvider value={theme}>{children}</ThemeProvider>;
 }
 
 export default Layout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,24 @@ const config: Config = withMT({
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          // 50: "#f0f9ff",
+          // 100: "#e0f2fe",
+          // 200: "#bae6fd",
+          // 300: "#7dd3fc",
+          // 400: "#38bdf8",
+          500: "#003459",
+          // 600: "#0284c7",
+          // 700: "#0369a1",
+          // 800: "#075985",
+          // 900: "#0c4a6e",
+        },
+      },
+    },
+  },
   plugins: [],
 });
 
