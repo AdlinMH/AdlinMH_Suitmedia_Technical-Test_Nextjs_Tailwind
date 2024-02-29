6e36b49eb907d1096ca5b36d0cde5680203992eb
Adlin Mohamad Hasri <adlin@rushowl.sg>
2024-03-01 00:30:02 +0700

    style: update font - complete the fonts weight setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,8 @@
 import "../styles/globals.css";
+
 import type { Metadata } from "next";
-// import { Roboto } from "next/font/google";
 import { Layout } from "@/components";
-
-// const roboto = Roboto({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "500", "700", "900"],
-//   display: "swap",
-// });
-
-import localFont from 'next/font/local'
-
-// Font files can be colocated inside of `app`
-const myFont = localFont({
-  src: [
-    {
-      path: '../styles/fonts/Gilroy-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../styles/fonts/Gilroy-RegularItalic.ttf',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../styles/fonts/Gilroy-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../styles/fonts/Gilroy-BoldItalic.ttf',
-      weight: '700',
-      style: 'italic',
-    },
-  ],
-  display: 'swap',
-  variable: '--font-gilroy',
-})
+import { myFont } from '@/styles/local-font'
 
 export const metadata: Metadata = {
   title: "NextJS Tailwi nd Course Landing Page",
---
+++ src/styles/local-font.tsx
@@ -0,0 +1,105 @@
+import localFont from 'next/font/local'
+// import { Roboto } from "next/font/google";
+
+// export const roboto = Roboto({
+//   subsets: ["latin"],
+//   weight: ["300", "400", "500", "700", "900"],
+//   display: "swap",
+// });
+
+export const myFont = localFont({
+  src: [
+    {
+      path: './fonts/Gilroy-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-ThinItalic.ttf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-UltraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-UltraLightItalic.ttf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-RegularItalic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-ExtraBoldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: './fonts/Gilroy-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Gilroy-BlackItalic.ttf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-gilroy',
+})
