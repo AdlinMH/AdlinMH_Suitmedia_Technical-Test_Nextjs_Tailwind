d7e78fe5ef60bc7561c7babe623d47070b0fba5f
Adlin Mohamad Hasri <adlin@rushowl.sg>
2024-03-01 00:13:05 +0700

    style: setup typography theme - primary

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
 import buttonTheme from '@/themes/button'
+import typographyTheme from '@/themes/typography'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const theme = {
     button: { ...buttonTheme },
+    typography: { ...typographyTheme },
   };
 
   return <ThemeProvider value={theme}>{children}</ThemeProvider>;
